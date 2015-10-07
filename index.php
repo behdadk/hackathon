927c7a595ec564f50f164755ab6c6bfa34c17127
Kevin van Cleef <kevin@developmentstudio.nl>
2015-10-07 14:32:11 +0200

    Adds /splittest resource to create new split tests.

--- index.php
+++ index.php
@@ -6,21 +6,27 @@ $app = new \Slim\Slim();
 $app->config("debug", $config["debugMode"]);
 $app->config("config", $config);
 
+/** @var \Twig_Environment $twig */
+$twig = $app->config("config")["twig"];
+/** @var \PDO $pdo */
+$pdo = $app->config("config")["pdo"];
 
-$app->get('/', function () use ($app) {
-    /** @var \Twig_Environment $twig */
-    $twig = $app->config("config")["twig"];
-
-    /** @var \PDO $pdo */
-    /*$pdo = $app->config("config")["pdo"];
-    $statement = $pdo->prepare("SHOW TABLES;");
-    $statement->execute();
-    $result = $statement->fetchAll();
-    var_dump($result);*/
-
-
+$app->get('/', function () use ($app, $twig) {
     $template = $twig->loadTemplate('coolblue-static.html');
     echo $template->render(array());
+});
+
+$app->post("/splittest", function () use ($app, $twig, $pdo) {
+    $json = $app->request->getBody();
+
+    $content = json_decode($json, true);
+
+    $statement = $pdo->prepare("INSERT INTO splittest (URL, ElementID) VALUES (:url, :element_id)");
+    $statement->bindParam(":url", $content["splittest"]["url"], \PDO::PARAM_STR);
+    $statement->bindParam(":element_id", $content["splittest"]["elementID"], \PDO::PARAM_STR);
+    $statement->execute();
+
+    echo json_encode($pdo->lastInsertId());
 });
 
 $app->run();
